db7cef5a7880ff91231cd6a4d468856ec34532e5
liull-4r <fetamasr@gmail.com>
2023-12-30 20:31:59 +0300

    cloned

--- src/Components/Row/RowCollect.jsx
+++ src/Components/Row/RowCollect.jsx
@@ -1,6 +1,6 @@
 import Banner from "../Banner/Banner";
 import Nav from "../Nav/Nav";
-import requests from "../Request/RequsetObject";
+import request from "../Request/RequsetObject";
 import Row from "./Row";
 
 function RowCollect() {
@@ -11,15 +11,15 @@ function RowCollect() {
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
+        fetchUrl={request.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
     </>
   );
 }
